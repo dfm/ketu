588ccb5d97d1261d92951342569c76d3e7766ee3
Dan F-M <danfm@nyu.edu>
2014-08-05 09:49:01 +0200

    plotting

--- demo.py
+++ demo.py
@@ -8,21 +8,22 @@ import matplotlib.pyplot as pl
 
 import turnstile
 
-r = 0.02
-dfreq = 0.005 / (4.25 * 365.)
-freq = np.arange(1.0 / 150., 1.0 / 50., dfreq)
-periods = 1.0 / freq
+r = 0.012
+pmin, pmax = 350., 400.
+periods = np.exp(np.arange(np.log(pmin), np.log(pmax), 0.3*0.3/(4.1*365.)))
 print("Testing {0} periods".format(len(periods)))
-durations = np.arange(0.2, 0.9, 0.1)
+durations = np.array([0.3])  # np.arange(0.3, 0.7, 0.2)
 print("Testing {0} durations".format(len(durations)))
-depths = np.arange(0.01, 0.07, 0.01) ** 2
+depths = np.array([0.01 ** 2])  # np.arange(0.01, 0.04, 0.02) ** 2
 print("Testing {0} depths".format(len(durations)))
 
-q = dict(
+q1 = dict(
     kicid=12253474,
-    injections=[dict(period=114.0, t0=22., radius=r)],
+    injections=[dict(period=365.25, t0=118.34, radius=r)],
+)
+q2 = dict(
     durations=durations.tolist(), depths=depths.tolist(),
-    periods=periods.tolist(), dt=0.1,
+    periods=periods.tolist(), dt=0.3 * 0.3,
 )
 
 pipe = turnstile.Download()
@@ -32,14 +33,16 @@ pipe = turnstile.Prepare(pipe)
 if False:
     suffix = "basic"
     pipe = turnstile.Detrend(pipe)
-    pipe = turnstile.BasicLikelihood(pipe)
+    pipe0 = turnstile.BasicLikelihood(pipe)
+    q1["detrend_window"] = 2
 else:
     suffix = "gp"
-    pipe = turnstile.GPLikelihood(pipe)
+    pipe0 = turnstile.GPLikelihood(pipe)
 
-pipe = turnstile.Hypotheses(pipe)
+pipe = turnstile.Hypotheses(pipe0)
 pipe = turnstile.Search(pipe)
 
+q = dict(q1, **q2)
 results = pipe.query(**q)
 
 z = results["grid"][:, :, 0, 0]
@@ -50,7 +53,7 @@ y = np.arange(0, z.shape[1] * results["dt"], results["dt"])
 X, Y = np.meshgrid(x, y, indexing="ij")
 
 pl.figure(figsize=(8, 8))
-pl.pcolormesh(X, Y, zimg, cmap="gray", vmin=-5000, vmax=3000)
+pl.pcolormesh(X, Y, zimg, cmap="gray")
 true_period = results["injection"].bodies[0].period
 true_t0 = (results["injection"].bodies[0].t0
            - results["mean_time"]) % true_period
@@ -67,10 +70,37 @@ pl.xlim(true_period - 20, true_period + 20)
 pl.ylim(true_t0 - 20, true_t0 + 20)
 pl.savefig("results/grid-{0}-zoom.png".format(suffix), dpi=300)
 
-pl.clf()
 z[np.isnan(z)] = -np.inf
+xi, yi = np.unravel_index(np.argmax(z), z.shape)
+period, t0 = x[xi], y[yi]
+print(xi, yi, x[xi], y[yi], z[xi, yi])
+
+# Plot the data.
+lcs = pipe0.query(**q1)["data"]
+fig, axes = pl.subplots(4, 4, figsize=(10, 10))
+t = (t0 + results["mean_time"]) % period
+i = 0
+for lc in lcs:
+    while t < lc.time.min():
+        t += period
+    m = np.abs(lc.time - t) < 3
+    if np.any(m):
+        axes.flat[i].plot(lc.time[m], lc.flux[m], ".k")
+        axes.flat[i].set_xlim(t - 3, t + 3)
+        axes.flat[i].set_xticklabels([])
+        axes.flat[i].set_yticklabels([])
+        i += 1
+        if i >= len(axes.flat):
+            break
+fig.savefig("results/transits-{0}.png".format(suffix))
+
+pl.figure()
 pl.plot(x, np.max(z, axis=1), "k")
 pl.gca().axvline(true_period, color="r", alpha=0.1, lw=3)
-pl.ylim(-500, 3000)
+# pl.ylim(-500, 3000)
 pl.xlabel("period")
 pl.savefig("results/periodogram-{0}.png".format(suffix))
+
+pl.plot(x, np.max(z, axis=1), ".k")
+pl.xlim(true_period - 0.5, true_period + 0.5)
+pl.savefig("results/periodogram-{0}-zoom.png".format(suffix))
