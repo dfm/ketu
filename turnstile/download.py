32e381aa42e38920d61dc8eed04175bbdd2846dd
Dan F-M <danfm@nyu.edu>
2014-09-06 19:33:35 -0400

    tarfile based download

--- turnstile/download.py
+++ turnstile/download.py
@@ -13,20 +13,22 @@ class Download(Pipeline):
 
     query_parameters = {
         "kicid": (None, True),
+        "tarfile_root": (None, True),
+        "data_root": (None, True),
         "short_cadence": (False, False),
         "npredictor": (50, False),
     }
 
     def get_result(self, query, parent_response):
         # Connect to the API.
-        client = kplr.API()
+        client = kplr.API(data_root=query["data_root"])
         kicid = query["kicid"]
         kic = client.star(kicid)
         kic.kois
 
         # Download the light curves.
         short_cadence = query["short_cadence"]
-        data = kic.get_light_curves(short_cadence=short_cadence, fetch=True)
+        data = kic.get_light_curves(short_cadence=short_cadence)
         if not len(data):
             raise ValueError("No light curves for KIC {0}"
                              .format(query["kicid"]))
@@ -37,7 +39,7 @@ class Download(Pipeline):
         q = dict(
             ktc_kepler_id="!={0:d}".format(kicid),
             ra=kic.kic_degree_ra, dec=kic.kic_dec, radius=1000,
-            ktc_target_type="LC", max_records=npredictor, fetch=True,
+            ktc_target_type="LC", max_records=npredictor,
         )
         predictor_lcs = []
         for lc in data:
@@ -46,5 +48,15 @@ class Download(Pipeline):
             q["sci_data_quarter"] = lc.sci_data_quarter
             predictor_lcs += [client.light_curves(**q)]
 
+        # Work out all of the KIC IDs that we'll touch.
+        kicids = (set("{0:09d}".format(int(kicid)))
+                  & set(lc.kicid for quarter in predictor_lcs
+                        for lc in quarter))
+        print(kicids)
+        assert 0
+
         return dict(star=kic, target_datasets=data,
                     predictor_datasets=predictor_lcs)
+
+    def _extract_light_curves(self, kicid, tarfile_root):
+        pass
